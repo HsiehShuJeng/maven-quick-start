4ffd458fc52438de0903320441d34e61f73309f9
scott.hsieh <fantasticSie@hotmail.com>
2017-05-10 17:22:48 +0800

    Use StringUtils and added dependency to lang3

--- src/main/java/scott/programming/training/Application.java
+++ src/main/java/scott/programming/training/Application.java
@@ -2,8 +2,15 @@ package scott.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application{
+    
+    public int countWords(String words){
+        String[] separateWords = StringUtils.split(words, ' ');
+        return (separateWords == null) ? 0 : separateWords.length;
+    }
+    
     public void greet(){
         List<String> greetings = new ArrayList<>();
         greetings.add("Hello");
@@ -20,5 +27,7 @@ public class Application{
         System.out.println("Starting Application");
         Application app = new Application();
         app.greet();
+        int count = app.countWords("“Data-driven” startups are the wildcatters of the new economy: they prospect for digital oil, extract it and turn it into clever new services, from analysing X-rays and CAT scans to determining where to spray herbicide on a field.");
+        System.out.println("Word Count: " + count);
     }
 }
